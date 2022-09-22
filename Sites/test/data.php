4f3b10ef8ebda288114d0187bab301c80df68378
mjmj112233 <palermarcusj@gmail.com>
2022-09-22 14:18:59 +0800

    login-signup

---
+++ Sites/test/data.php
@@ -0,0 +1,28 @@
+<?php
+
+return [
+  [
+      'title' => 'The Wedding', 
+      'tags' => 'romance, action',
+      'author' => 'Kim Jisoo',
+      'email' => 'email1@email.com',
+      'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+      'ratings' => '4/5'
+  ],
+  [
+    'title' => 'The Diary of a Wimpy Kid', 
+    'tags' => 'comedy, kids',
+    'author' => 'Jung Hae In',
+    'email' => 'email2@email.com',
+    'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+    'ratings' => '3/5'
+],
+[
+    'title' => 'Lebron', 
+    'tags' => 'sport, thriller',
+    'author' => 'James',
+    'email' => 'email3@email.com',
+    'content' => 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsam minima et illo reprehenderit quas possimus voluptas repudiandae cum expedita, eveniet aliquid, quam illum quaerat consequatur! Expedita ab consectetur tenetur delensiti?',
+    'ratings' => '2/5'
+]
+];
