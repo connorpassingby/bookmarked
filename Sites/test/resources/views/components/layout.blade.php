bba8bd7c0c1d91cba6148b355626d1a443db9917
connorpassingby <connorpassingby@gmail.com>
2022-10-07 20:05:15 +0800

    Update layout.blade.php

--- Sites/test/resources/views/components/layout.blade.php
+++ Sites/test/resources/views/components/layout.blade.php
@@ -86,7 +86,7 @@
             <p class="ml-2">Group 3 Software Engineering 1 | Copyright &copy; 2022, All Rights reserved</p>
 
             <a
-                href="create.html"
+                href="/reviews/create"
                 class="absolute top-1/3 right-10 bg-black text-white py-2 px-5"
                 >Post a Review</a
             >
