ea286b1809f0617841a323b92f78b22b5e2c9d7c
connorpassingby <connorpassingby@gmail.com>
2022-10-14 06:56:49 +0800

    Update ReviewController.php

--- Sites/bookmarked/app/Http/Controllers/ReviewController.php
+++ Sites/bookmarked/app/Http/Controllers/ReviewController.php
@@ -4,7 +4,6 @@ namespace App\Http\Controllers;
 
 use App\Models\Review;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rule;
 use App\Http\Controllers\Controller;
 
 class ReviewController extends Controller
