cff2915bc3d467679bdfd384e6b6508bfb6d4834
connorpassingby <connorpassingby@gmail.com>
2022-10-14 08:04:16 +0800

    Update DatabaseSeeder.php

--- Sites/bookmarked/database/seeders/DatabaseSeeder.php
+++ Sites/bookmarked/database/seeders/DatabaseSeeder.php
@@ -30,16 +30,16 @@ class DatabaseSeeder extends Seeder
             'user_id' => $user->id
         ]);
         
-        // Review::create([
-        //     'title' => 'Les Miserables',
-        //     'author' => 'Victor Hugo',
-        //     'media_type' => 'Novel',
-        //     'genre' => 'Historical Fiction',
-        //     'tags' => 'Major Character Death, Graphic Violence, Adapted into a Musical, Romance',
-        //     'summary' => 'Jean Valjean, a former criminal, gets a second chance at an honest life. Under the alias of \"Monsieur Madeleine\", he rises to great heights as the mayor of Montreuil-sur-Mer and even starts a family. However, his past returns to haunt him as his identity as a criminal is uncovered. Les Miserables is the story of one man\'s chance at redemption and reconciliation with family amidst the turmoil of the French Revolution.',
-        //     'reviewer' => 'Nico Marcelino',
-        //     'description' => 'Les Miserables is a good story but why did the author have to kill off all the major characters? Furthermore, what was the point of describing the Parisian sewers in great detail? It\'s not relevant at all to the story or to the setting...',
-        // ]);
+        Review::create([
+            'user_id' => $user->id,
+            'title' => 'Les Miserables',
+            'author' => 'Victor Hugo',
+            'media_type' => 'Novel',
+            'genre' => 'Historical Fiction',
+            'tags' => 'Major Character Death, Graphic Violence, Adapted into a Musical, Romance',
+            'summary' => 'Jean Valjean, a former criminal, gets a second chance at an honest life. Under the alias of \"Monsieur Madeleine\", he rises to great heights as the mayor of Montreuil-sur-Mer and even starts a family. However, his past returns to haunt him as his identity as a criminal is uncovered. Les Miserables is the story of one man\'s chance at redemption and reconciliation with family amidst the turmoil of the French Revolution.',
+            'description' => 'Les Miserables is a good story but why did the author have to kill off all the major characters? Furthermore, what was the point of describing the Parisian sewers in great detail? It\'s not relevant at all to the story or to the setting...',
+        ]);
 
         // Review::create([
         //     'title' => 'The Wedding',
